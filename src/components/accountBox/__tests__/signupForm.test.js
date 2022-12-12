e6303bbfdd095836549dfdb4fa7f6f9ad7fef7b7
Zahra947 <85116470+Zahra947@users.noreply.github.com>
2022-12-12 17:31:29 +0330

    1 file modified, 1 deleted

--- src/components/accountBox/__tests__/signupForm.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-import signupForm from '../signupForm';
-import 'jest-styled-components';
-
-test('it works', () => {
-  const tree = renderer.create(<signupForm />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
